e67782f839e32dbfb740c8eacf442afeeba70c89
keunwoochoi <gnuchoi+github@gmail.com>
2016-03-07 17:42:23 +0000

    change to space indentation

--- classifier_blocks.py
+++ classifier_blocks.py
@@ -7,63 +7,63 @@ from keras.activations import relu
 
 
 def building_classifier_block(setting_dict, input_shape):
-	''''''
-	num_labels = setting_dict["dim_labels"]
-	num_fc_layers = setting_dict["num_fc_layers"]
-	dropouts_fc_layers = setting_dict["dropouts_fc_layers"]
-	nums_units_fc_layers = setting_dict["nums_units_fc_layers"]
-	nb_feature = 4 # for maxout
-	#--------------------------------------#
-	block = keras.layers.containers.Graph()
+    ''''''
+    num_labels = setting_dict["dim_labels"]
+    num_fc_layers = setting_dict["num_fc_layers"]
+    dropouts_fc_layers = setting_dict["dropouts_fc_layers"]
+    nums_units_fc_layers = setting_dict["nums_units_fc_layers"]
+    nb_feature = 4 # for maxout
+    #--------------------------------------#
+    block = keras.layers.containers.Graph()
 
-	# set input shape
-	input_name = 'classifier_input' 
-	block.add_input(input_name, input_shape=input_shape)
-	prev_output = input_name
-	
-	# Flatten
-	layer_name = 'flatten'
-	block.add_node(Flatten(input_shape=input_shape), 
-					name=layer_name,
-					input=prev_output)
-	last_node_name = layer_name
-	
-	# Maxout layers and conv
-	for fc_idx in xrange(num_fc_layers):
-		print 'Add fc layer %d' % fc_idx
-		this_node_name = 'maxout_%d' % fc_idx
-		
-		block.add_node(MaxoutDense(nums_units_fc_layers[fc_idx], 
-									nb_feature=nb_feature),
-						input=last_node_name,
-						name=this_node_name)
-		last_node_name = this_node_name
-		# Dropout
-		if not dropouts_fc_layers[fc_idx] == 0.0:
-			this_node_name = 'dropout_fc_%d' % fc_idx
-			block.add_node(Dropout(dropouts_fc_layers[fc_idx]),
-							input=last_node_name,
-							name=this_node_name)
-			last_node_name = this_node_name
+    # set input shape
+    input_name = 'classifier_input' 
+    block.add_input(input_name, input_shape=input_shape)
+    prev_output = input_name
+    
+    # Flatten
+    layer_name = 'flatten'
+    block.add_node(Flatten(input_shape=input_shape), 
+                    name=layer_name,
+                    input=prev_output)
+    last_node_name = layer_name
+    
+    # Maxout layers and conv
+    for fc_idx in xrange(num_fc_layers):
+        print 'Add fc layer %d' % fc_idx
+        this_node_name = 'maxout_%d' % fc_idx
+        
+        block.add_node(MaxoutDense(nums_units_fc_layers[fc_idx], 
+                                    nb_feature=nb_feature),
+                        input=last_node_name,
+                        name=this_node_name)
+        last_node_name = this_node_name
+        # Dropout
+        if not dropouts_fc_layers[fc_idx] == 0.0:
+            this_node_name = 'dropout_fc_%d' % fc_idx
+            block.add_node(Dropout(dropouts_fc_layers[fc_idx]),
+                            input=last_node_name,
+                            name=this_node_name)
+            last_node_name = this_node_name
 
-		this_node_name = 'bn_fc_%d' % fc_idx
-		block.add_node(BatchNormalization(axis=1),
-										input=last_node_name,
-										name=this_node_name)
-		last_node_name = this_node_name
+        this_node_name = 'bn_fc_%d' % fc_idx
+        block.add_node(BatchNormalization(axis=1),
+                                        input=last_node_name,
+                                        name=this_node_name)
+        last_node_name = this_node_name
 
-	# final layer
-	this_node_name = 'last_dense'
-	block.add_node(Dense(num_labels, activation='sigmoid'), 
-					input=last_node_name, 
-					name=this_node_name)
-	last_node_name = this_node_name
-	
-	#[Output layer]
-	this_node_name = 'output'
-	block.add_output(input=last_node_name,
-					name=this_node_name)
+    # final layer
+    this_node_name = 'last_dense'
+    block.add_node(Dense(num_labels, activation='sigmoid'), 
+                    input=last_node_name, 
+                    name=this_node_name)
+    last_node_name = this_node_name
+    
+    #[Output layer]
+    this_node_name = 'output'
+    block.add_output(input=last_node_name,
+                    name=this_node_name)
 
-	# return it
-	return block
+    # return it
+    return block
 
--- example.py
+++ example.py
@@ -33,104 +33,104 @@ import sys
 
 
 def design_for_residual_blocks():
-	''''''
-	model = keras.layers.containers.Sequential() # it's a CONTAINER, not MODEL. It should be replaced with Sequential
-	# set numbers
-	image_patch_sizes = [[3,3]]*num_layers
-	vgg_modi_weight, pool_sizes = get_residual_weights(num_layers=num_layers)
-	setting_dict['vgg_modi_weight'] = vgg_modi_weight
-	height_input = height
-	width_input = width
-	num_channel_input = 1
+    ''''''
+    model = keras.layers.containers.Sequential() # it's a CONTAINER, not MODEL. It should be replaced with Sequential
+    # set numbers
+    image_patch_sizes = [[3,3]]*num_layers
+    vgg_modi_weight, pool_sizes = get_residual_weights(num_layers=num_layers)
+    setting_dict['vgg_modi_weight'] = vgg_modi_weight
+    height_input = height
+    width_input = width
+    num_channel_input = 1
 
-	for conv_idx in range(num_layers):
-		print '= design for mel - conv layer blocks %d will be added = ' % conv_idx
-	
-		n_feat_here = int(num_stacks[conv_idx]*vgg_modi_weight[conv_idx][0])
-		
-		# residual block 0
-		this_node_name = 'residual_block_%d_0' % conv_idx
-		name_prefix = 'Conv_%d_0' % conv_idx
-		model.add(my_residual_blocks.building_residual_block(name_prefix,
-															input_shape=(num_channel_input, height_input, width_input),
-															n_feature_maps=n_feat_here,
-															kernel_sizes=image_patch_sizes[conv_idx]
-															))
-		last_node_name = this_node_name
+    for conv_idx in range(num_layers):
+        print '= design for mel - conv layer blocks %d will be added = ' % conv_idx
+    
+        n_feat_here = int(num_stacks[conv_idx]*vgg_modi_weight[conv_idx][0])
+        
+        # residual block 0
+        this_node_name = 'residual_block_%d_0' % conv_idx
+        name_prefix = 'Conv_%d_0' % conv_idx
+        model.add(my_residual_blocks.building_residual_block(name_prefix,
+                                                            input_shape=(num_channel_input, height_input, width_input),
+                                                            n_feature_maps=n_feat_here,
+                                                            kernel_sizes=image_patch_sizes[conv_idx]
+                                                            ))
+        last_node_name = this_node_name
 
-		# residual block 1
-		this_node_name = 'residual_block_%d_1' % conv_idx
-		name_prefix = 'Conv_%d_1' % conv_idx
-		model.add(my_residual_blocks.building_residual_block(name_prefix,
-															input_shape=(n_feat_here, height_input, width_input),
-															n_feature_maps=n_feat_here,
-															kernel_sizes=image_patch_sizes[conv_idx]
-															))
-		last_node_name = this_node_name
-		# residual block 2
-		this_node_name = 'residual_block_%d_2' % conv_idx
-		name_prefix = 'Conv_%d_2' % conv_idx
-		model.add(my_residual_blocks.building_residual_block(name_prefix,
-															input_shape=(n_feat_here, height_input, width_input),
-															n_feature_maps=n_feat_here,
-															kernel_sizes=image_patch_sizes[conv_idx]
-															))
-		last_node_name = this_node_name
-		
-		# residual block 3
-		# the last one : subsamples and increase #channels
-		this_node_name = 'residual_block_%d_3' % conv_idx
-		try:
-			n_feat_next = int(num_stacks[conv_idx+1]*vgg_modi_weight[conv_idx+1][0])
-		except:
-			pass
-		name_prefix = 'Conv_%d_3' % conv_idx
-		model.add(my_residual_blocks.building_residual_block(name_prefix,
-															input_shape=(n_feat_here, height_input, width_input),
-															n_feature_maps=n_feat_next,
-															kernel_sizes=image_patch_sizes[conv_idx],
-															is_subsample=True,
-															subsample=pool_sizes[conv_idx]
-															))
-		last_node_name = this_node_name
+        # residual block 1
+        this_node_name = 'residual_block_%d_1' % conv_idx
+        name_prefix = 'Conv_%d_1' % conv_idx
+        model.add(my_residual_blocks.building_residual_block(name_prefix,
+                                                            input_shape=(n_feat_here, height_input, width_input),
+                                                            n_feature_maps=n_feat_here,
+                                                            kernel_sizes=image_patch_sizes[conv_idx]
+                                                            ))
+        last_node_name = this_node_name
+        # residual block 2
+        this_node_name = 'residual_block_%d_2' % conv_idx
+        name_prefix = 'Conv_%d_2' % conv_idx
+        model.add(my_residual_blocks.building_residual_block(name_prefix,
+                                                            input_shape=(n_feat_here, height_input, width_input),
+                                                            n_feature_maps=n_feat_here,
+                                                            kernel_sizes=image_patch_sizes[conv_idx]
+                                                            ))
+        last_node_name = this_node_name
+        
+        # residual block 3
+        # the last one : subsamples and increase #channels
+        this_node_name = 'residual_block_%d_3' % conv_idx
+        try:
+            n_feat_next = int(num_stacks[conv_idx+1]*vgg_modi_weight[conv_idx+1][0])
+        except:
+            pass
+        name_prefix = 'Conv_%d_3' % conv_idx
+        model.add(my_residual_blocks.building_residual_block(name_prefix,
+                                                            input_shape=(n_feat_here, height_input, width_input),
+                                                            n_feature_maps=n_feat_next,
+                                                            kernel_sizes=image_patch_sizes[conv_idx],
+                                                            is_subsample=True,
+                                                            subsample=pool_sizes[conv_idx]
+                                                            ))
+        last_node_name = this_node_name
 
-		height_input /= pool_sizes[conv_idx][0]
-		width_input  /= pool_sizes[conv_idx][1]
-		num_channel_input = n_feat_next
+        height_input /= pool_sizes[conv_idx][0]
+        width_input  /= pool_sizes[conv_idx][1]
+        num_channel_input = n_feat_next
 
-	this_node_name = 'residual_block_output'
-	# model.add_output(name=this_node_name, input=last_node_name)
+    this_node_name = 'residual_block_output'
+    # model.add_output(name=this_node_name, input=last_node_name)
 
-	return model
+    return model
 
 def design_residual_model(setting_dict):
-	''''''
-	#-------------- design_residual_model -------------------#
-	#--------------------------------------------------------#
-	n_skips = setting_dict['n_skips'] # These are the setting I'm using.
-	tf_type = setting_dict['tf_type'] 
-	height = setting_dict["height_image"]
-	width = setting_dict["width_image"]
-	num_labels = setting_dict["dim_labels"]
-	num_layers = setting_dict["num_layers"]
-	num_fc_layers = setting_dict["num_fc_layers"]
-	dropouts_fc_layers = setting_dict["dropouts_fc_layers"]
-	nums_units_fc_layers = setting_dict["nums_units_fc_layers"]
-	num_stacks = setting_dict["num_feat_maps"]
-	dropouts_fc_layers = setting_dict["dropouts_fc_layers"]
-	num_channels = 1
-	nb_maxout_feature = setting_dict['nb_maxout_feature']
-	#--------------------------------------------------------#
-	sys.setrecursionlimit(99999)
-	# start the model!
-	model = keras.models.Sequential() # 
-	# [residual-based Conv layers]
-	residual_blocks = design_for_residual_blocks()
-	model.add(residual_blocks)
-	# [Prepare to add classifier]
-	residual_output_shape = residual_blocks.output_shape
-	classifier_input_shape = residual_output_shape[1:]
-	# [Classifier]
-	model.add(my_classifier_blocks.building_classifier_block(setting_dict, classifier_input_shape))
-	# [END]
-	return model
+    ''''''
+    #-------------- design_residual_model -------------------#
+    #--------------------------------------------------------#
+    n_skips = setting_dict['n_skips'] # These are the setting I'm using.
+    tf_type = setting_dict['tf_type'] 
+    height = setting_dict["height_image"]
+    width = setting_dict["width_image"]
+    num_labels = setting_dict["dim_labels"]
+    num_layers = setting_dict["num_layers"]
+    num_fc_layers = setting_dict["num_fc_layers"]
+    dropouts_fc_layers = setting_dict["dropouts_fc_layers"]
+    nums_units_fc_layers = setting_dict["nums_units_fc_layers"]
+    num_stacks = setting_dict["num_feat_maps"]
+    dropouts_fc_layers = setting_dict["dropouts_fc_layers"]
+    num_channels = 1
+    nb_maxout_feature = setting_dict['nb_maxout_feature']
+    #--------------------------------------------------------#
+    sys.setrecursionlimit(99999)
+    # start the model!
+    model = keras.models.Sequential() # 
+    # [residual-based Conv layers]
+    residual_blocks = design_for_residual_blocks()
+    model.add(residual_blocks)
+    # [Prepare to add classifier]
+    residual_output_shape = residual_blocks.output_shape
+    classifier_input_shape = residual_output_shape[1:]
+    # [Classifier]
+    model.add(my_classifier_blocks.building_classifier_block(setting_dict, classifier_input_shape))
+    # [END]
+    return model
